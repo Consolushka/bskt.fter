02d73341993c79a19ffed811a8b2403193fddef9
Vladislav Lukin <consolushka@gmail.com>
2025-02-20 14:45:47 +0300

    feat(abstract http client): added generic to get request

--- app/internal/abstract/http/client.go
+++ app/internal/abstract/http/client.go
@@ -12,6 +12,34 @@ func NewHttpClient() *Client {
 	return &Client{}
 }
 
+func Get[T any](url string, apiKey *string) T {
+	var result T
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("accept", "application/json")
+	if apiKey != nil {
+		req.Header.Add("Authorization", *apiKey)
+	}
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(res.Body)
+	body, _ := io.ReadAll(res.Body)
+
+	err := json.Unmarshal(body, &result)
+	if err != nil {
+		panic(err)
+	}
+
+	return result
+}
+
 // Get send GET request to given url w/o body and headers (yet)
 //
 //todo:use better abstraction
