a99d2ca69126ef728d6f4c46bdf34f706926ba41
Vladislav Lukin <capkerby@gmail.com>
2025-05-15 10:06:59 +0300

    refactor(translator): refactored translator to struct

--- app/internal/statistics/translator/translator.go
+++ app/internal/statistics/translator/translator.go
@@ -4,9 +4,19 @@ import (
 	gt "github.com/bas24/googletranslatefree"
 )
 
+type Interface interface {
+	Translate(source string, sourceLangPoint *string, targetLang string) string
+}
+
+type translator struct{}
+
+func NewTranslator() Interface {
+	return &translator{}
+}
+
 // Translate translates the given text to the target language.
 // If the source language is not specified, it will be automatically detected.
-func Translate(source string, sourceLangPoint *string, targetLang string) string {
+func (t *translator) Translate(source string, sourceLangPoint *string, targetLang string) string {
 	var sourceLang string
 
 	if sourceLangPoint == nil {
