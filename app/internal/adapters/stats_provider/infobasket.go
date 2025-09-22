fcfeecbbaa8b61d0c5e4ec420ef8a1b11892c571
Vladislav Lukin <capkerby@gmail.com>
2025-09-22 16:54:19 +0300

    feat(infobasket): integrate Infobasket stats provider and entity transformer
    - Added `InfobasketStatsProviderAdapter` for fetching and transforming game stats.
    - Introduced `EntityTransformer` within the `infobasket` package to handle game, team, and player transformations.
    - Updated `GetGamesStatsByDate` to utilize Infobasket services for game data retrieval and processing.

--- app/internal/adapters/stats_provider/infobasket.go
+++ app/internal/adapters/stats_provider/infobasket.go
@@ -2,13 +2,49 @@ package stats_provider
 
 import (
 	"IMP/app/internal/core/games"
-	"errors"
+	"IMP/app/internal/infra/infobasket"
+	"fmt"
+	"strconv"
 	"time"
 )
 
 type InfobasketStatsProviderAdapter struct {
+	client infobasket.ClientInterface
+
+	transformer infobasket.EntityTransformer
+	compId      int
 }
 
 func (i InfobasketStatsProviderAdapter) GetGamesStatsByDate(date time.Time) ([]games.GameStatEntity, error) {
-	return nil, errors.New("CDN_NBA infobasket")
+	var gamesEntities []games.GameStatEntity
+	schedule, err := i.client.ScheduledGames(i.compId)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, game := range schedule {
+		if game.GameDate == date.Format("02.01.2006") {
+			gameBoxScore, err := i.client.BoxScore(strconv.Itoa(game.GameID))
+			if err != nil {
+				// todo: log
+				fmt.Println("There was an error while fetching game box score", ". GameID: ", strconv.Itoa(game.GameID), ". Error: ", err)
+			}
+
+			transform, err := i.transformer.Transform(gameBoxScore)
+			if err != nil {
+				// todo: log
+				fmt.Println("There was an error while transforming game box score", ". GameID: ", strconv.Itoa(game.GameID), ". Error: ", err)
+			}
+
+			gamesEntities = append(gamesEntities, transform)
+		}
+	}
+	return gamesEntities, nil
+}
+
+func NewInfobasketStatsProviderAdapter(client infobasket.ClientInterface, compId int) InfobasketStatsProviderAdapter {
+	return InfobasketStatsProviderAdapter{
+		client: client,
+		compId: compId,
+	}
 }
---
+++ app/internal/infra/infobasket/entity_transformer.go
@@ -0,0 +1,70 @@
+package infobasket
+
+import (
+	"IMP/app/internal/core/games"
+	"IMP/app/internal/core/players"
+	"IMP/app/internal/core/teams"
+	"fmt"
+	"time"
+)
+
+type EntityTransformer struct{}
+
+func (e *EntityTransformer) Transform(game GameBoxScoreResponse) (games.GameStatEntity, error) {
+	parse, err := time.Parse("02.01.2006 15.04", game.GameDate+" "+game.GameTimeMsk)
+	if err != nil {
+		return games.GameStatEntity{}, err
+	}
+
+	return games.GameStatEntity{
+		GameModel: games.GameModel{
+			ScheduledAt: parse,
+			Title:       game.GameTeams[0].TeamName.CompTeamAbcNameEn + " - " + game.GameTeams[1].TeamName.CompTeamAbcNameEn,
+		},
+		HomeTeamStat: e.transformTeam(game.GameTeams[0]),
+		AwayTeamStat: e.transformTeam(game.GameTeams[1]),
+	}, nil
+}
+
+func (e *EntityTransformer) transformTeam(team TeamBoxScoreDto) teams.TeamStatEntity {
+	playerStats := make([]players.PlayerStatisticEntity, len(team.Players))
+
+	for i, player := range team.Players {
+		playerStat, err := playersTrans(player)
+		if err != nil {
+			// todo: log
+			fmt.Println("There was an error with player statistics. Error: ", err, " . Player: ", player.PersonID, " ", player.PersonNameEn)
+		}
+
+		playerStats[i] = playerStat
+	}
+
+	return teams.TeamStatEntity{
+		TeamModel: teams.TeamModel{
+			Name:     team.TeamName.CompTeamNameEn,
+			HomeTown: team.TeamName.CompTeamRegionNameEn,
+		},
+		GameTeamStatModel: teams.GameTeamStatModel{
+			Score: team.Score,
+		},
+		PlayerStats: playerStats,
+	}
+}
+
+func playersTrans(playerStat PlayerBoxScoreDto) (players.PlayerStatisticEntity, error) {
+	parsedBirth, err := time.Parse("02.01.2006", playerStat.PersonBirth)
+	if err != nil {
+		return players.PlayerStatisticEntity{}, err
+	}
+
+	return players.PlayerStatisticEntity{
+		PlayerModel: players.PlayerModel{
+			FullName:  playerStat.PersonNameEn,
+			BirthDate: parsedBirth,
+		},
+		GameTeamPlayerStatModel: players.GameTeamPlayerStatModel{
+			PlayedSeconds: playerStat.Seconds,
+			PlsMin:        int8(playerStat.PlusMinus),
+		},
+	}, nil
+}
