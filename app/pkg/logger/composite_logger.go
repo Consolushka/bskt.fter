c41d9ca0fb9895cd07a49bb95927b081d49a1475
Vladislav Lukin <capkerby@gmail.com>
2025-09-25 14:30:58 +0300

    hotfix: fix logger

--- app/pkg/logger/composite_logger.go
+++ app/pkg/logger/composite_logger.go
@@ -2,32 +2,32 @@ package logger
 
 import "IMP/app/internal/ports"
 
-var logger CompositeLogger
+var instance CompositeLogger
 
 type CompositeLogger struct {
 	loggers []ports.Logger
 }
 
 func Init(loggers []ports.Logger) {
-	logger = CompositeLogger{
+	instance = CompositeLogger{
 		loggers: loggers,
 	}
 }
 
 func Info(msg string, ctx map[string]interface{}) {
-	for _, logger := range logger.loggers {
+	for _, logger := range instance.loggers {
 		logger.Info(msg, ctx)
 	}
 }
 
 func Warn(msg string, ctx map[string]interface{}) {
-	for _, logger := range logger.loggers {
+	for _, logger := range instance.loggers {
 		logger.Warn(msg, ctx)
 	}
 }
 
 func Error(msg string, ctx map[string]interface{}) {
-	for _, logger := range logger.loggers {
+	for _, logger := range instance.loggers {
 		logger.Error(msg, ctx)
 	}
 }
