3ae91546568206b49e3f4dd4442db744e3bbd12c
Vladislav Lukin <capkerby@gmail.com>
2025-10-03 17:43:50 +0300

    feat(tournaments_repo): add method to list tournaments by league aliases

--- app/internal/adapters/tournaments_repo/gorm.go
+++ app/internal/adapters/tournaments_repo/gorm.go
@@ -2,12 +2,27 @@ package tournaments_repo
 
 import (
 	"IMP/app/internal/core/tournaments"
+	"strings"
 
 	"gorm.io/gorm"
 )
 
 type GormRepo struct {
 	db *gorm.DB
+}
+
+func (u GormRepo) ListTournamentsByLeagueAliases(aliases []string) ([]tournaments.TournamentModel, error) {
+	var models []tournaments.TournamentModel
+	conditionLeaguesAliases := "("
+
+	for _, alias := range aliases {
+		conditionLeaguesAliases += "'" + alias + "',"
+	}
+	conditionLeaguesAliases = strings.TrimRight(conditionLeaguesAliases, ",") + ")"
+
+	err := u.db.Model(&tournaments.TournamentModel{}).Preload("League").Preload("ExternalIds").InnerJoins("left join leagues on tournaments.league_id = leagues.id").Find(&models, "leagues.alias in "+conditionLeaguesAliases).Error
+
+	return models, err
 }
 
 func (u GormRepo) FindTournamentExternalId(tournamentId uint, providerName string) (tournaments.TournamentExternalIdModel, error) {
