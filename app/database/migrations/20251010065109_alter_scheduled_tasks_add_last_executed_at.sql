b5151e12d22d56ac5c9704a9b16ba98ec1625d54
Vladislav Lukin <capkerby@gmail.com>
2025-10-10 17:02:39 +0300

    feat: add `last_executed_at` column to ScheduledTaskModel and database schema

---
+++ app/database/migrations/20251010065109_alter_scheduled_tasks_add_last_executed_at.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+-- +goose StatementBegin
+alter table scheduled_tasks
+    add column last_executed_at timestamptz default date_subtract(now(), interval '1 day');
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+alter table scheduled_tasks
+    drop column last_executed_at;
+-- +goose StatementEnd
