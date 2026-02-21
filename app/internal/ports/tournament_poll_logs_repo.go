4346cdb8bcb394ee6d17dd9955bcccc4c2667f3c
Vladislav Lukin <capkerby@gmail.com>
2026-02-21 10:44:21 +0300

    feat(poll_logs): add database migration, core model and repo port for poll logs

--- app/internal/ports/tournament_poll_logs_repo.go
+++ app/internal/ports/tournament_poll_logs_repo.go
@@ -4,7 +4,7 @@ import (
 	"IMP/app/internal/core/tournament_poll_logs"
 )
 
-type PollLogRepo interface {
+type TournamentPollLogsRepo interface {
 	Create(log tournament_poll_logs.TournamentPollLogModel) (tournament_poll_logs.TournamentPollLogModel, error)
 	GetLatestSuccess(tournamentId uint) (tournament_poll_logs.TournamentPollLogModel, error)
 }
