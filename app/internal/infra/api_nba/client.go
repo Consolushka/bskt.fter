2f2f9414592420a2581d54e0401c9977b4a0c618
Vladislav Lukin <capkerby@gmail.com>
2026-04-10 11:16:49 +0300

    feat(infra): implement dynamic URL parameter handling in API_NBA client

--- app/internal/infra/api_nba/client.go
+++ app/internal/infra/api_nba/client.go
@@ -3,6 +3,7 @@ package api_nba
 import (
 	"IMP/app/pkg/http"
 	"context"
+	"net/url"
 	"os"
 	"strconv"
 	"time"
@@ -42,22 +43,96 @@ func NewClient(baseUrl string, token string) ClientInterface {
 }
 
 func (c Client) Games(id int, date string, leagueId string, season string, teamId string, timezone string) (GamesResponse, error) {
-	_ = c.limiter.Wait(context.Background())
-	return http.Get[GamesResponse](c.baseUrl+"/games?date="+date, &c.baseHeaders)
+	if c.limiter != nil {
+		_ = c.limiter.Wait(context.Background())
+	}
+
+	params := url.Values{}
+	if id != 0 {
+		params.Add("id", strconv.Itoa(id))
+	}
+	if date != "" {
+		params.Add("date", date)
+	}
+	if leagueId != "" {
+		params.Add("league", leagueId)
+	}
+	if season != "" {
+		params.Add("season", season)
+	}
+	if teamId != "" {
+		params.Add("team", teamId)
+	}
+	if timezone != "" {
+		params.Add("timezone", timezone)
+	}
+
+	query := ""
+	if len(params) > 0 {
+		query = "?" + params.Encode()
+	}
+
+	return http.Get[GamesResponse](c.baseUrl+"/games"+query, &c.baseHeaders)
 }
 
 func (c Client) PlayersStatistics(playerId int, gameId int, teamId int, season string) (PlayerStatisticResponse, error) {
-	//todo: ignore zero values
-	//
 	// For free plan limit is 10 requests/minute
-	_ = c.limiter.Wait(context.Background())
-	return http.Get[PlayerStatisticResponse](c.baseUrl+"/players/statistics?game="+strconv.Itoa(gameId), &c.baseHeaders)
+	if c.limiter != nil {
+		_ = c.limiter.Wait(context.Background())
+	}
+
+	params := url.Values{}
+	if playerId != 0 {
+		params.Add("id", strconv.Itoa(playerId))
+	}
+	if gameId != 0 {
+		params.Add("game", strconv.Itoa(gameId))
+	}
+	if teamId != 0 {
+		params.Add("team", strconv.Itoa(teamId))
+	}
+	if season != "" {
+		params.Add("season", season)
+	}
+
+	query := ""
+	if len(params) > 0 {
+		query = "?" + params.Encode()
+	}
+
+	return http.Get[PlayerStatisticResponse](c.baseUrl+"/players/statistics"+query, &c.baseHeaders)
 }
 
 func (c Client) PlayerInfo(playerId int, name string, teamId int, season int, country string, search string) (PlayersResponse, error) {
-	//todo: ignore zero values
-	//
 	// For free plan limit is 10 requests/minute
-	_ = c.limiter.Wait(context.Background())
-	return http.Get[PlayersResponse](c.baseUrl+"/players?id="+strconv.Itoa(playerId), &c.baseHeaders)
+	if c.limiter != nil {
+		_ = c.limiter.Wait(context.Background())
+	}
+
+	params := url.Values{}
+	if playerId != 0 {
+		params.Add("id", strconv.Itoa(playerId))
+	}
+	if name != "" {
+		params.Add("name", name)
+	}
+	if teamId != 0 {
+		params.Add("team", strconv.Itoa(teamId))
+	}
+	if season != 0 {
+		params.Add("season", strconv.Itoa(season))
+	}
+	if country != "" {
+		params.Add("country", country)
+	}
+	if search != "" {
+		params.Add("search", search)
+	}
+
+	query := ""
+	if len(params) > 0 {
+		query = "?" + params.Encode()
+	}
+
+	return http.Get[PlayersResponse](c.baseUrl+"/players"+query, &c.baseHeaders)
 }
